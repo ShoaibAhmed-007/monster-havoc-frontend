16bc13ec2377c54bfa64fd64696c8c8dca66389c
Shoaib <xshoaibahmedx007@gmail.com>
2024-08-18 17:07:06 +0500

    responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,10 +4,10 @@ import { motion } from "framer-motion";
 function Navbar() {
   return (
     <>
-      <div className="w-screen flex justify-between items-center bg-[rgba(0,0,0,0.9)] py-4 px-2 text-white xl:text-lg lg:text-base md:text-sm sm:text-xs">
+      <div className="w-screen flex justify-between items-center bg-[rgba(0,0,0,0.9)] py-4 px-2 text-white xl:text-lg lg:text-base md:text-sm sm:text-xs max-sm:text-[9px]">
         <div className="flex justify-around items-center w-[35%]">
           <img
-            className="xl:h-12 lg:h-10 md:h-8 sm:h-6"
+            className="xl:h-12 lg:h-10 md:h-8 sm:h-6 max-sm:h-6"
             src="/images/logo.png"
             alt="logo"
           />
@@ -19,7 +19,7 @@ function Navbar() {
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
             transition={{ type: "spring", damping: 10 }}
-            className="rounded-full border-2 border-white px-8 py-2"
+            className="rounded-full border-2 border-white xl:px-8 xl:py-2 py-1 lg:px-6 md:px-4 sm:px-3 max-sm:px-2"
           >
             Signup
           </motion.button>
@@ -27,7 +27,7 @@ function Navbar() {
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
             transition={{ type: "spring", damping: 10 }}
-            className="rounded-full border-2 border-white px-8 py-2"
+            className="rounded-full border-2 border-white xl:px-8 xl:py-2 py-1 lg:px-6 md:px-4 sm:px-3 max-sm:px-2"
           >
             Login
           </motion.button>
