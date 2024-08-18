9892ebd1b09a2e74c3c079705660413d801480dc
zeddspear <zeddspear@gmail.com>
2024-08-18 15:00:37 +0500

    App Layout setting

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,4 @@
+function Navbar() {
+  return <div></div>;
+}
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Hero from "./components/Hero/Hero";
 
 export default function Home() {
   return (
     <>
-      <div></div>
+      <div>
+        <Hero />
+      </div>
     </>
   );
 }
