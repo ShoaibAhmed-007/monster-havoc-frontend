e24eff7dd045c9d0361f93eaff132dba9a20324a
Shoaib <xshoaibahmedx007@gmail.com>
2024-08-25 01:17:36 +0500

    Api calls

--- app/Pages/monsters/page.tsx
+++ app/Pages/monsters/page.tsx
@@ -1,14 +1,129 @@
+"use client";
 import Card from "@/app/components/Card";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+type statsType = {
+  attack: Number;
+  defense: Number;
+  speed: Number;
+  health: Number;
+};
+
+export type monsterType = {
+  _id: string;
+  name: string;
+  img: string;
+  type: string;
+  abilities: string[];
+  stats: statsType;
+  description: string;
+  evolution: string;
+};
 
 function page() {
+  const [monsters, setMonsters] = useState<monsterType[] | null>(null);
+  const [lockedMonsters, setLockedMonsters] = useState<monsterType[]>([]);
+  async function getUserMonsters() {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/getUserMonsters`,
+        { withCredentials: true }
+      );
+      console.log(response.data.monsters);
+      setMonsters(response.data.monsters);
+    } catch (error: any) {
+      if (error.response) {
+        // Server responded with a status other than 2xx
+        console.error("Error response:", error.response);
+        // Handle specific error codes if needed
+        if (error.response.status === 404) {
+          console.error("Monsters not found.");
+        } else if (error.response.status === 500) {
+          console.error("Server error. Please try again later.");
+        } else {
+          console.error("An error occurred:", error.response.data.message);
+        }
+      } else if (error.request) {
+        // Request was made but no response was received
+        console.error("No response received:", error.request);
+        console.error("Please check your network connection.");
+      } else {
+        // Something else caused the error
+        console.error("Error:", error.message);
+      }
+    }
+  }
+
+  async function getAllMonsters() {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/getAllMonsters`,
+        { withCredentials: true }
+      );
+      let locked = response.data.monsters.filter((monster: monsterType) => {
+        // Check if this monster's ID exists in the monsters array
+        const isLocked = !monsters?.some((userMonst: monsterType) => {
+          const match = userMonst._id === monster._id;
+          console.log(
+            `Comparing ${userMonst._id} with ${monster._id}: ${match}`
+          );
+          return match;
+        });
+
+        console.log(`Monster ID: ${monster._id}, isLocked: ${isLocked}`);
+        return isLocked;
+      });
+
+      console.log(locked);
+      setLockedMonsters(locked);
+    } catch (error: any) {
+      if (error.response) {
+        // Server responded with a status other than 2xx
+        console.error("Error response:", error.response);
+        // Handle specific error codes if needed
+        if (error.response.status === 404) {
+          console.error("Monsters not found.");
+        } else if (error.response.status === 500) {
+          console.error("Server error. Please try again later.");
+        } else {
+          console.error("An error occurred:", error.response.data.message);
+        }
+      } else if (error.request) {
+        // Request was made but no response was received
+        console.error("No response received:", error.request);
+        console.error("Please check your network connection.");
+      } else {
+        // Something else caused the error
+        console.error("Error:", error.message);
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (!monsters) {
+      getUserMonsters();
+      getAllMonsters();
+    }
+  }, [monsters]);
+
   return (
     <>
       <div className="flex justify-center items-center py-10">
         <div className="bg-black bg-opacity-50 p-5 flex flex-col items-center">
-          <h1>Your Monsters</h1>
           <div className="bg-black bg-opacity-70 h-96 w-96">
-            {/* <Card /> */}
+            <div>
+              <h1>Your Monsters</h1>
+              {monsters?.map((monster: monsterType, idx) => {
+                return <Card monster={monster} />;
+              })}
+            </div>
+            <div>
+              <h1>Locked Monsters</h1>
+              {lockedMonsters.map((monster: monsterType, idx) => {
+                return <Card monster={monster} />;
+              })}
+            </div>
           </div>
         </div>
       </div>
--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,68 +1,60 @@
-// import React from "react";
-// import Image from "next/image";
-// import { FaHeart, FaShieldAlt, FaBolt, FaFistRaised } from "react-icons/fa"; // Font Awesome icons
+import React from "react";
+import Image from "next/image";
+import { FaHeart, FaShieldAlt, FaBolt, FaFistRaised } from "react-icons/fa"; // Font Awesome icons
+import { monsterType } from "../Pages/monsters/page";
 
-// type statsType = {
-//   attack: Number;
-//   defense: Number;
-//   speed: Number;
-//   health: Number;
-// };
+type cardProps = {
+  monster: monsterType;
+};
 
-// type monsterType = {
-//   name: string;
-//   img: string;
-//   type: string;
-//   abilities: string[];
-//   stats: statsType;
-//   description: string;
-//   evolution: string;
-// };
+function Card({ monster }: cardProps) {
+  return (
+    <div className="min-w-64 max-w-64 flex flex-col gap-2 p-4 bg-gray-800 rounded-lg shadow-lg border-2 border-yellow-500">
+      <div className="text-center text-white font-bold text-lg">
+        {monster?.name}
+      </div>
+      <div className="relative">
+        <Image
+          height={220}
+          width={220}
+          src={monster?.img}
+          alt="Pyrofang"
+          className="rounded-lg"
+        />
+      </div>
+      <div className="flex flex-col gap-1 text-white">
+        <div className="text-center">
+          <span className="font-bold">Type:</span> {monster?.type}
+        </div>
+        <div className="flex flex-col w-full gap-1">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-1">
+              <FaHeart className="text-red-500" />
+              <span className="font-bold">Health:</span>{" "}
+              {monster?.stats?.health?.toString()}
+            </div>
+            <div className="flex items-center gap-1">
+              <FaFistRaised className="text-yellow-500" />
+              <span className="font-bold">Attack:</span>{" "}
+              {monster?.stats?.attack?.toString()}
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-1">
+              <FaShieldAlt className="text-blue-500" />
+              <span className="font-bold">Defense:</span>{" "}
+              {monster?.stats?.defense?.toString()}
+            </div>
+            <div className="flex items-center gap-1">
+              <FaBolt className="text-green-500" />
+              <span className="font-bold">Speed:</span>{" "}
+              {monster?.stats?.speed?.toString()}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-// function Card({ monster }: monsterType) {
-//   return (
-//     <div className="min-w-64 max-w-64 flex flex-col gap-2 p-4 bg-gray-800 rounded-lg shadow-lg border-2 border-yellow-500">
-//       <div className="text-center text-white font-bold text-lg">
-//         {monster.name}
-//       </div>
-//       <div className="relative">
-//         <Image
-//           height={220}
-//           width={220}
-//           src={monster.img}
-//           alt="Pyrofang"
-//           className="rounded-lg"
-//         />
-//       </div>
-//       <div className="flex flex-col gap-1 text-white">
-//         <div className="text-center">
-//           <span className="font-bold">Type:</span> {monster.type}
-//         </div>
-//         <div className="flex flex-col w-full gap-1">
-//           <div className="flex justify-between items-center">
-//             <div className="flex items-center gap-1">
-//               <FaHeart className="text-red-500" />
-//               <span className="font-bold">Health:</span> {monster.health}
-//             </div>
-//             <div className="flex items-center gap-1">
-//               <FaFistRaised className="text-yellow-500" />
-//               <span className="font-bold">Attack:</span> {monster.attack}
-//             </div>
-//           </div>
-//           <div className="flex justify-between items-center">
-//             <div className="flex items-center gap-1">
-//               <FaShieldAlt className="text-blue-500" />
-//               <span className="font-bold">Defense:</span> {monster.defense}
-//             </div>
-//             <div className="flex items-center gap-1">
-//               <FaBolt className="text-green-500" />
-//               <span className="font-bold">Speed:</span> {monster.speed}
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Card;
+export default Card;
